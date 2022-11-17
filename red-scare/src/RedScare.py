00b1d6166a0f4c64c4a014dab97f7c65d2298237
bjal <bjal@itu.dk>
2022-11-17 16:07:21 +0100

    fix segfault

--- red-scare/src/RedScare.py
+++ red-scare/src/RedScare.py
@@ -1,7 +1,7 @@
-
-import sys
 from math import inf
-from collections import deque, defaultdict
+from collections import defaultdict
+from sys import setrecursionlimit, argv
+setrecursionlimit(10**6)
 
 class UnionFind:
   def __init__(self, V):
@@ -28,181 +28,149 @@ class UnionFind:
     return p
 
 
-
 def is_cyclic(g, s):
-    import sys
-    sys.setrecursionlimit(10**6)
-    def dfs_cyclic(v, path, seen):
-        if v in seen: 
-            return False
-        seen.add(v)
-        for u, w in g[v].items():
-            # if we took u -> v then discard v -> u 
-            if len(path) > 1 and u == path[-2]: 
-                continue
-            if u in path: 
-                return True 
-            path.append(u)
-            if dfs_cyclic(u, path, seen): 
-                return True
-            path.pop()
-        return False
-    return dfs_cyclic(s, [s], set())
-
+  def dfs_cyclic(v, path, seen):
+    if v in seen: 
+      return False
+    seen.add(v)
+    for u, w in g[v].items():
+      # if we took u -> v then discard v -> u 
+      if len(path) > 1 and u == path[-2]: 
+        continue
+      if u in path: 
+        return True 
+      path.append(u)
+      if dfs_cyclic(u, path, seen): 
+        return True
+      path.pop()
+    return False
+  return dfs_cyclic(s, [s], set())
 
 
 def shortest_path(g, s, t):
-    from heapq import heappush, heappop
+  from heapq import heappush, heappop
 
-    path_to = {s: s}
-    dist_to = {s: 0}
-    queue = [(s, 0)]
+  path_to = {s: s}
+  dist_to = {s: 0}
+  queue = [(s, 0)]
 
-    while queue:
-        v, d = heappop(queue)
-        for u,w in g[v].items():
-            if path_to[v] == u: continue
-            if u not in dist_to or dist_to[u] > dist_to[v] + w: 
-                dist_to[u] = dist_to[v] + w
-                path_to[u] = v
-                heappush(queue, (u, dist_to[u]))
+  while queue:
+    v, d = heappop(queue)
+    for u,w in g[v].items():
+      if path_to[v] == u: continue
+      if u not in dist_to or dist_to[u] > dist_to[v] + w: 
+        dist_to[u] = dist_to[v] + w
+        path_to[u] = v
+        heappush(queue, (u, dist_to[u]))
 
-    if t not in dist_to:
-        return None, None
+  if t not in dist_to:
+    return None, None
 
-    dist = dist_to[t]
-    path = [] 
-    while t != s: 
-        path.append(t)
-        t = path_to[t]
+  dist = dist_to[t]
+  path = [] 
+  while t != s: 
     path.append(t)
+    t = path_to[t]
+  path.append(t)
 
-    return dist, list(reversed(path))
-
+  return dist, list(reversed(path))
 
 
 def all_paths(g, s, t):
-    import sys
-    sys.setrecursionlimit(10**6)
-    def dfs_paths(v, path):
-        for u, w in g[v].items():
-            # if we took u -> v then discard v -> u 
-            if len(path) > 1 and u == path[-2]: continue
-            if u in path: continue 
+  def dfs_paths(v, path):
+    for u, w in g[v].items():
+      # if we took u -> v then discard v -> u 
+      if len(path) > 1 and u == path[-2]: continue
+      if u in path: continue 
 
-            path.append(u)
-            if u == t: yield list(path)
-            yield from dfs_paths(u, path)
-            path.pop()
+      path.append(u)
+      if u == t: yield list(path)
+      yield from dfs_paths(u, path)
+      path.pop()
 
-    return dfs_paths(s, [s])
+  return dfs_paths(s, [s])
 
-
-def fdfs(graph, source, sink, lvl, flow, seen):
+def fdfs(graph, source, sink, seen, flow, thresh):
   if source == sink:
     return flow
   if source not in seen:
     seen.add(source)
     for nxt,cap in graph[source].items():
-      if cap >= 1 and source in lvl and nxt in lvl and lvl[source] < lvl[nxt]:
-        delta = fdfs(graph, nxt, sink, lvl, min(flow, cap), seen)
-        if delta >= 1:
+      if source not in graph[nxt]: return 0 # only use bidirectional
+      if cap >= thresh:
+        delta = fdfs(graph, nxt, sink, seen, min(flow, cap), thresh)
+        if delta >= thresh:
           graph[source][nxt] -= delta
           graph[nxt][source] += delta
           return delta
   return 0
 
-def lvlbfs(g, s, t):
-  lvl = {s:0}
-  q = deque([(s,0)])
-  while q:
-    v,vlvl = q.pop()
-    if v == t: 
-      lvl[v] = vlvl + 1
-      break
-    for w,c in g[v].items():
-      if w in lvl or c == 0: continue 
-      lvl[w] = vlvl + 1
-      q.appendleft((w, vlvl + 1))
-  return lvl
+def maxflow(graph, source, sink, thresh=1):
+  INF = 2 ** 31
+  flow = 0
 
-def maxflow(G, source, sink, flow=0):
-  levels = lvlbfs(G, source, sink)
-  if sink not in levels: return flow
-  increase = inf
-  while increase > 0:
-    increase = fdfs(G,source,sink,levels,inf,set())
-    flow += increase
-  return maxflow(G, source, sink, flow=flow) 
+  while thresh > 0: 
+    delta = INF
+    while delta > 0:
+      delta = fdfs(graph, source, sink, set(), INF, thresh)
+      flow += delta
+    thresh = thresh//2
+
+  return flow
 
 
 
 def redscare_none(G, R, s, t):
-    G = {
-        v: {u: w for u, w in es.items() if u == s or u == t or u not in R} 
-        for v, es in G.items() if v == s or v == t or not v in R
-    } 
+  G = {
+    v: {u: w for u, w in es.items() if u == s or u == t or u not in R} 
+    for v, es in G.items() if v == s or v == t or not v in R
+  } 
 
-    dist, path = shortest_path(G, s, t)
-    return dist
+  dist, path = shortest_path(G, s, t)
+  return dist
 
 def redscare_alternate(G, R, s, t):
-    G = {
-        v: {u: w for u, w in es.items() if (u in R) != (v in R) } 
-        for v, es in G.items()
-    } 
+  G = {
+    v: {u: w for u, w in es.items() if (u in R) != (v in R) } 
+    for v, es in G.items()
+  } 
 
-    return any(shortest_path(G, s, t))
+  return any(shortest_path(G, s, t))
 
 def redscare_few(G, R, s, t):
-    G = {
-        v: {u: (1 if u in R else 0) for u, w in es.items() }
-        for v, es in G.items()
-    }
-    dist, path = shortest_path(G, s, t)
-    return dist
+  G = {
+    v: {u: (1 if u in R else 0) for u, w in es.items() }
+    for v, es in G.items()
+  }
+  dist, path = shortest_path(G, s, t)
+  return dist
 
 def redscare_many(G, R, s, t):
-    G = {
-        v: {u: (-1 if u in R else 0) for u, w in es.items() }
-        for v, es in G.items()
-    }
-    dist, path = shortest_path(G, s, t)
-    return -dist if dist is not None else None
+  G = {
+    v: {u: (-1 if u in R else 0) for u, w in es.items() }
+    for v, es in G.items()
+  }
+  dist, path = shortest_path(G, s, t)
+  return -dist if dist is not None else None
 
 def redscare_many_brute(G, R, s, t):
-    paths = all_paths(G,s,t)
-    if paths:
-        return max([sum([1 if v in R else 0 for v in path]) for path in all_paths(G,s,t)])
-    else:
-        return None
+  paths = all_paths(G,s,t)
+  if paths:
+    return max([sum([1 if v in R else 0 for v in path]) for path in all_paths(G,s,t)])
+  else:
+    return None
 
 def redscare_some(G, R, s, t, N):
-  G = {
-    v: {u: 1 for u in es.keys() if v in G[u]}
-    for v,es in G.items()
-  }
-
-  G_Split = defaultdict(dict)
-
-  for i in range(N): 
-    G_Split[i][N+i] = 1
-    G_Split[N+i][i] = 0
-
-  for v,es in G.items():
-    for u in es.keys():
-      G_Split[N+v][u] = 1
-
-  G = G_Split
-
   source = -1
+  G[source] = {}
   G[source][s] = 1
   G[s][source] = 0
   G[source][t] = 1
-  G[t][source] = 1
+  G[t][source] = 0
 
   for r in R:
-    if maxflow(G, source, r) >= 2:
+    if maxflow({v:{u:w for u,w in es.items()} for v,es in G.items()}, source, r) >= 2:
+      print('true')
       return True
   return None
 
@@ -227,17 +195,11 @@ for i in range(m):
   G[V[u]][V[v]] = 1
   if e != '->': G[V[v]][V[u]] = 1
 
-if len(sys.argv) > 2 and sys.argv[2] == 'large' and len(G) < 500:
-    exit()
-if len(sys.argv) > 2 and sys.argv[2] == 'small' and len(G) > 200:
-    exit()
-
-
 s,t = V[s], V[t]
 # Is s and t in the same component?
 if UF.find(s) != UF.find(t): 
   print()
-  print(sys.argv[1], end='\t')
+  print(argv[1], end='\t')
   print(n, end='\t')
   print('false', end='\t')
   print('-1', end='\t')
@@ -267,27 +229,27 @@ few = few_dist if few_dist else '-1'
 none_dist = redscare_none(G, R, s, t)
 none = none_dist if none_dist else '-1'
 
-some = 'true' if redscare_some(G, R, s, t, n) else None
+some = 'true' if redscare_some(G, R, s, t, n) is not None else None
 
 if len(R) == 0:
-    many_dist = redscare_none(G, R, s, t)
-    many = 0 if many_dist is not None else '-1'
-    if some is None: some = 'false'
+  many_dist = redscare_none(G, R, s, t)
+  many = 0 if many_dist is not None else '-1'
+  if some is None: some = 'false'
 elif not is_cyclic(G, s):
-    many_dist = redscare_many(G, R, s, t)
-    many = many_dist if many_dist is not None else '-1'
-    if some is None and many_dist is not None: some = 'false' if many_dist == 0 else 'true'
+  many_dist = redscare_many(G, R, s, t)
+  many = many_dist if many_dist is not None else '-1'
+  if some is None and many_dist is not None: some = 'false' if many_dist == 0 else 'true'
 elif len(G) < brute_force_limit: # constant time algorithm, factor 14!
-    many_dist = redscare_many_brute(G, R, s, t)
-    many = many_dist
-    if some is None and many_dist is not None: some = 'false' if many_dist == 0 else 'true'
+  many_dist = redscare_many_brute(G, R, s, t)
+  many = many_dist
+  if some is None and many_dist is not None: some = 'false' if many_dist == 0 else 'true'
 else:
-    many = '?!'
+  many = '?!'
 some = '?!' if some is None else some
 
 
 print()
-print(sys.argv[1], end='\t')
+print(argv[1], end='\t')
 print(n, end='\t')
 print('true' if alternate else 'false', end='\t')
 print(few, end='\t')
